6215c15890d72d1ce9ed030954ea0019e36a1eb3
exhnil <g.confiac@live.fr>
2026-02-24 15:14:05 +0100

    Correction of previous commit

--- src/main/java/com/exhnil/arkenstone/controllers/UserController.java
+++ src/main/java/com/exhnil/arkenstone/controllers/UserController.java
@@ -1,7 +1,6 @@
 package com.exhnil.arkenstone.controllers;
 
 import com.exhnil.arkenstone.dto.UserCredentials;
-import com.exhnil.arkenstone.dto.UserDTO;
 import com.exhnil.arkenstone.entities.UserEntity;
 import com.exhnil.arkenstone.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +28,7 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public UserEntity saveUser(@RequestBody UserDTO user) {
-        return userService.saveUser(user);
+    public UserEntity saveUser(@RequestBody UserCredentials request) {
+        return userService.saveUser(request.getEmail(), request.getPassword());
     }
 }
